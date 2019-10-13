b7ff56ed518b489faa0ddd1bc8e3a95e4133348a
Jordan Mosio <jordan.mosio@hotmail.fr>
2019-10-13 10:12:45 +0200

    [Feature] Pouvoir copier le text d'un entrainement.

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,24 +1,37 @@
 var nextPageNumber = 1;
 
 /**
- * Auto select the training content when double clicking on it.
+ * Copy the next training text to the clip board.
  */
-function autoSelectTraining() {
-	var sel, range;
-	var el = $(this)[0];
-	if (window.getSelection && document.createRange) { // Browser compatibility
-		sel = window.getSelection();
-		window.setTimeout(function() {
-			range = document.createRange(); // range object
-			range.selectNodeContents(el); // sets Range
-			sel.removeAllRanges(); // remove all ranges from selection
-			sel.addRange(range);// add Range to a Selection.
-		}, 1);
-	} else if (document.selection) { // older ie
-		sel = document.selection.createRange();
-		range = document.body.createTextRange();// Creates TextRange object
-		range.moveToElementText(el);// sets Range
-		range.select(); // make selection.
+function copyText() {
+	var button = $(this);
+	var content = button.parent().next("pre");
+	
+	if (window.clipboardData) { // For IE.
+		window.clipboardData.setData("Text", content.text());
+	} else {
+		var tmpElem = $('<div>');
+		tmpElem.css({position: "absolute", left: "-1000px", top: "-1000px"});
+		tmpElem.append('<pre>' + content.text() + '</pre>');
+		$("body").append(tmpElem);
+		
+		var range = document.createRange();
+		range.selectNodeContents(tmpElem.get(0));
+		
+		var selection;
+		selection = window.getSelection();
+		selection.removeAllRanges();
+		selection.addRange(range);
+
+		success = document.execCommand ("copy", false, null);
+		if (success) {
+			button.attr("title", "Text copié !");
+			button.tooltip('show');
+			tmpElem.remove();
+			setTimeout(() => {
+				button.tooltip('hide');
+			}, 3000);
+		}
 	}
 }
 
@@ -36,7 +49,6 @@ function getTrainingColDiv(training) {
 	trainingDiv.addClass("p-3 bg-light rounded border border-dark");
 	
 	var content = $("<pre></pre>");
-	content.dblclick(autoSelectTraining);
 	content.text(training.text);
 	
 	var par = typeof training.coach === 'undefined' ? "" : '<span class="badge badge-dark p-2 ml-1">' + training.coach.name + "</span>";
@@ -47,7 +59,13 @@ function getTrainingColDiv(training) {
 	}
 	var size = '<span class="badge badge-info p-2">' + training.size + "m</span>";
 	
+	var copyDiv = $('<div class="text-right"></div>');
+	var imgCopy = $('<img class="btn btn-light" data-toggle="tooltip" width="50px" src="resources/images/copy.png" />');
+	imgCopy.click(copyText);
+	copyDiv.append(imgCopy);
+	
 	trainingDiv.append('<h5 class="text-center pb-1">' + training.dateSeanceString + "</h5>");
+	trainingDiv.append(copyDiv);
 	trainingDiv.append(content);
 	trainingDiv.append('<div class="text-right">' + size + par + bassin + "</div>");
 	
