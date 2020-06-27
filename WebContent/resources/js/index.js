0f3394fa0af2f7d5d330a35bcc436bae015355fd
Jordan Mosio <jordan.mosio@hotmail.fr>
2020-06-27 11:44:06 +0200

    [Technique] Identation.

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -3,38 +3,38 @@
  */
 function loadTrainings() {
 
-	startLoadingAnimation();
-	$.get(  "public/service/search",
-			{
-				order:	 	"date_seance desc, updatedAt desc",
-				limite:		4,
-				page:		1
-			}
-	).done(function (data) {
-		
-		var rawData = JSON.parse(data);
-		var jsonData = rawData.message.trainings;
-		var nbResultPerPage = rawData.message.maxResultPerPage;
-		var total = rawData.message.totalNbOfResults;
-		
-		var row = $("<div></div>");
-		row.addClass("row justify-content-start");
-		$.each(jsonData, function(i, training) {
-			var col = getTrainingColDiv(training, rawData.canModify, rawData.isAdmin);
-			col.addClass("px-0 px-xl-1");
-			col.hide().fadeIn();
-			row.append(col);
-		});
-		$("#resArea").append(row);
-		
-		var isAdmin = rawData.isAdmin;
-		if (isAdmin) {
-			$("#menu_index").append('<div class="col-auto text-center"><a href="admin/admin.html" class="img"><img width="150px" src="resources/images/admin.png" /></a></div>');
-		}
-		
-		stopLoadingAnimation();
-	})
-	.fail(displayError);
+    startLoadingAnimation();
+    $.get(  "public/service/search",
+            {
+                order:	 	"date_seance desc, updatedAt desc",
+                limite:		4,
+                page:		1
+            }
+    ).done(function (data) {
+
+        var rawData = JSON.parse(data);
+        var jsonData = rawData.message.trainings;
+        var nbResultPerPage = rawData.message.maxResultPerPage;
+        var total = rawData.message.totalNbOfResults;
+
+        var row = $("<div></div>");
+        row.addClass("row justify-content-start");
+        $.each(jsonData, function(i, training) {
+            var col = getTrainingColDiv(training, rawData.canModify, rawData.isAdmin);
+            col.addClass("px-0 px-xl-1");
+            col.hide().fadeIn();
+            row.append(col);
+        });
+        $("#resArea").append(row);
+
+        var isAdmin = rawData.isAdmin;
+        if (isAdmin) {
+            $("#menu_index").append('<div class="col-auto text-center"><a href="admin/admin.html" class="img"><img width="150px" src="resources/images/admin.png" /></a></div>');
+        }
+
+        stopLoadingAnimation();
+    })
+    .fail(displayError);
 }
 
 // Affichage de la page, on affiche les entrainements
