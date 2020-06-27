53423facd2b67f1b4efd781c142dc3812556ebcc
Jordan Mosio <jordan.mosio@hotmail.fr>
2020-06-27 11:43:06 +0200

    [BugFix] Affichage du créateur uniquement s'il existe.

--- WebContent/resources/js/common.js
+++ WebContent/resources/js/common.js
@@ -1,65 +1,65 @@
 function displayError(xhr, status, error) {
-	var errorMessage = xhr.status + ': ' + xhr.statusText + " (" + error + ")";
-	stopLoadingAnimation();
-	alert('Une erreur est survenue - ' + errorMessage);
+    var errorMessage = xhr.status + ': ' + xhr.statusText + " (" + error + ")";
+    stopLoadingAnimation();
+    alert('Une erreur est survenue - ' + errorMessage);
 }
 
 /** Stops a valid timer. */
 function clearTimer(pTimeOut) {
-	if (typeof pTimeOut === 'number') {
-		window.clearTimeout(pTimeOut);
-	}
+    if (typeof pTimeOut === 'number') {
+        window.clearTimeout(pTimeOut);
+    }
 }
 
 //Fonctions de chargement
 var timeoutId = null;
 var $body = $("body");
 function doTheLoading() {
-	$body.addClass("loading");
+    $body.addClass("loading");
 }
 function startLoadingAnimation() {
-	clearTimer(timeoutId);
-	timeoutId = window.setTimeout(doTheLoading, 300);
+    clearTimer(timeoutId);
+    timeoutId = window.setTimeout(doTheLoading, 300);
 }
 function stopLoadingAnimation() {
-	clearTimer(timeoutId);
-	$body.removeClass("loading");
+    clearTimer(timeoutId);
+    $body.removeClass("loading");
 }
 
 /**
  * Copy the next training text to the clip board.
  */
 function copyText() {
-	var button = $(this);
-	var content = button.parent().parent().find("pre");
-	
-	if (window.clipboardData) { // For IE.
-		window.clipboardData.setData("Text", content.text());
-	} else {
-		var tmpElem = $('<div>');
-		tmpElem.css({position: "absolute", left: "-1000px", top: "-1000px"});
-		tmpElem.append('<pre></pre>');
-		tmpElem.find('pre').text(content.text());
-		$("body").append(tmpElem);
-		
-		var range = document.createRange();
-		range.selectNodeContents(tmpElem.get(0));
-		
-		var selection;
-		selection = window.getSelection();
-		selection.removeAllRanges();
-		selection.addRange(range);
+    var button = $(this);
+    var content = button.parent().parent().find("pre");
 
-		success = document.execCommand ("copy", false, null);
-		if (success) {
-			button.attr("title", "Text copié !");
-			button.tooltip('show');
-			tmpElem.remove();
-			setTimeout(() => {
-				button.tooltip('hide');
-			}, 3000);
-		}
-	}
+    if (window.clipboardData) { // For IE.
+        window.clipboardData.setData("Text", content.text());
+    } else {
+        var tmpElem = $('<div>');
+        tmpElem.css({position: "absolute", left: "-1000px", top: "-1000px"});
+        tmpElem.append('<pre></pre>');
+        tmpElem.find('pre').text(content.text());
+        $("body").append(tmpElem);
+
+        var range = document.createRange();
+        range.selectNodeContents(tmpElem.get(0));
+
+        var selection;
+        selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        success = document.execCommand ("copy", false, null);
+        if (success) {
+            button.attr("title", "Text copié !");
+            button.tooltip('show');
+            tmpElem.remove();
+            setTimeout(() => {
+                button.tooltip('hide');
+            }, 3000);
+        }
+    }
 }
 
 function deleteTraining() {
@@ -89,7 +89,7 @@ function deleteTraining() {
 function getTrainingColDiv(training, canModify, isAdmin) {
 
     // Layout
-	var cardContent = $(`
+    var cardContent = $(`
         <div class="p-3 bg-light rounded border border-dark h-100">
             <table class="h-100 w-100">
                 <tr>
@@ -115,45 +115,45 @@ function getTrainingColDiv(training, canModify, isAdmin) {
             </table>
         </div>`);
 
-	// Footer, action et contenu de la séance
-	var footer = cardContent.find(".training_footer");
-	cardContent.find("pre").text(training.text);
-	cardContent.find(`#copy-${training.id}`).click(copyText);
-	if (!canModify) {
-	    cardContent.find(`#modif-edit-${training.id}`).remove();
-	}
-	if (isAdmin) {
-	    cardContent.find(`#admin-delete-${training.id}`).click(deleteTraining);
-	} else {
-	    cardContent.find(`#admin-delete-${training.id}`).remove();
-	}
+    // Footer, action et contenu de la séance
+    var footer = cardContent.find(".training_footer");
+    cardContent.find("pre").text(training.text);
+    cardContent.find(`#copy-${training.id}`).click(copyText);
+    if (!canModify) {
+        cardContent.find(`#modif-edit-${training.id}`).remove();
+    }
+    if (isAdmin) {
+        cardContent.find(`#admin-delete-${training.id}`).click(deleteTraining);
+    } else {
+        cardContent.find(`#admin-delete-${training.id}`).remove();
+    }
 
     // Contenu du footer
-	var par = typeof training.coach === 'undefined' ? "" : '<span class="badge badge-dark p-2 ml-1">' + training.coach.name + "</span>";
-	var bassin = "";
-	var isLongCourse = training.isCourseSizeDefinedForSure && training.isLongCourse;
-	var tailleBassinText = training.isLongCourse ? "Grand Bain" : "Petit Bain";
-	var tailleBassinClass = training.isLongCourse ? "badge-warning" : "badge-primary";
-	if (training.isCourseSizeDefinedForSure) {
-		bassin = ` <span class="badge ${tailleBassinClass} p-2">${tailleBassinText}</span>`;
-	}
-	var size = `<span class="badge badge-info p-2">${training.size}m</span>`;
-	footer.html(size + par + bassin);
-	if (isAdmin) {
-	    footer.append(` <div class="font-italic">Ajouté par ${training.createdBy.email}, le ${training.createdAt}.</div>`);
-	}
+    var par = typeof training.coach === 'undefined' ? "" : '<span class="badge badge-dark p-2 ml-1">' + training.coach.name + "</span>";
+    var bassin = "";
+    var isLongCourse = training.isCourseSizeDefinedForSure && training.isLongCourse;
+    var tailleBassinText = training.isLongCourse ? "Grand Bain" : "Petit Bain";
+    var tailleBassinClass = training.isLongCourse ? "badge-warning" : "badge-primary";
+    if (training.isCourseSizeDefinedForSure) {
+        bassin = ` <span class="badge ${tailleBassinClass} p-2">${tailleBassinText}</span>`;
+    }
+    var size = `<span class="badge badge-info p-2">${training.size}m</span>`;
+    footer.html(size + par + bassin);
+    if (isAdmin && typeof training.createdBy != 'undefined') {
+        footer.append(` <div class="font-italic">Ajouté par ${training.createdBy.email}, le ${training.createdAt}.</div>`);
+    }
 
     // Main div
-	var trainingCol = $('<div></div>');
-	trainingCol.addClass("col-12 col-xl-6 my-2");
-	trainingCol.append(cardContent);
-	return trainingCol;
+    var trainingCol = $('<div></div>');
+    trainingCol.addClass("col-12 col-xl-6 my-2");
+    trainingCol.append(cardContent);
+    return trainingCol;
 }
 
 function formatDate(myDate) {
-	return myDate.getFullYear() 
-		+ "-"
-		+ ("0" + (myDate.getMonth() + 1)).slice(-2)
-		+ "-"
-		+ ("0" + myDate.getDate()).slice(-2);
+    return myDate.getFullYear()
+        + "-"
+        + ("0" + (myDate.getMonth() + 1)).slice(-2)
+        + "-"
+        + ("0" + myDate.getDate()).slice(-2);
 }
